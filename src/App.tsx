1bf160f0119e59c1b579ae1bdfab8461ad0fdeec
susanzh4ng <Susan.Zhang901@gmail.com>
2025-10-09 15:09:18 -0500

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import CountMap from './components/CountMap'
+import HelpRequestPost from './components/HelpRequestPost'
 import './App.css'
 import supabase from './supabase-client'
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <CountMap />
+      <HelpRequestPost />
     </>
   )
 }
---
+++ src/components/HelpRequestPost.tsx
@@ -0,0 +1,13 @@
+import { useEffect, useRef } from "react";
+//import supabase from "../supabase-client";
+
+const HelpRequestPost = () => {
+
+  return (
+    <div>
+        <h1>HelpRequestPost</h1>
+    </div>
+  );
+};
+
+export default HelpRequestPost;
