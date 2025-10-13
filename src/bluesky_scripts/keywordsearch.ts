4752d8dd30783db33fbcecfb622b8e7029cd3b24
Medha Kotra <mxk210117@utdallas.edu>
2025-10-13 10:52:26 -0500

    ran cleanup.ts script to delete non-English posts

--- src/bluesky_scripts/keywordsearch.ts
+++ src/bluesky_scripts/keywordsearch.ts
@@ -4,6 +4,7 @@ import { createClient } from '@supabase/supabase-js';
 import dotenv from 'dotenv';
 dotenv.config();
 
+
 // ---------------- Supabase Setup ----------------
 const supabase = createClient(
   process.env.SUPABASE_URL!,
