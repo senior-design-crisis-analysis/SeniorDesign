b627cbb9528004509439599d17780dba24bd782b
fati <87720728+fatima-carmona@users.noreply.github.com>
2025-10-16 18:14:19 -0500

    Update App.tsx
    vercel deployment fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+//import { useState, useEffect } from 'react'
 import CountMap from './components/CountMap'
 import './App.css'
 //import supabase from './supabase-client'
